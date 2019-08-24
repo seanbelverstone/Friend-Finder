80ff74a0bffa53a465df0412a39e0c27115247b5
Sean Belverstone <seanbelverstone@gmail.com>
2019-08-24 15:31:07 -0500

    Added in part of a previous exercise`s post method. Commented out as will visit later

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,21 @@
 var path = require("path");
 
-// Displays all characters
-app.get("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "../data.friends.js"));
-});
+// Shows the friends JSON object
+module.exports = function(app){
+    app.get("/api/friends", function(req, res) {
+        res.sendFile(path.join(__dirname, "../data/friends.js"));
+    });
+
+    // app.post("/api/friends", function(req, res) {
+
+    //     var matchingFriend = req.body;
+      
+    //     matchingFriend.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+      
+    //     console.log(newCharacter);
+      
+    //     characters.push(newCharacter);
+      
+    //     res.json(newCharacter);
+    //   });
+}
