4141c9b0c99112d76689ea6e5b86fe419ee2b630
Sean Belverstone <seanbelverstone@gmail.com>
2019-08-24 15:59:06 -0500

    Started creating my friends.js json object

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friends = [{
+    name: "Frodo Baggins",
+    photo: "",
+    answers: [
+
+    ]
+},
+{
+    name: "Samwise Gamgee",
+    photo: "",
+    answers: [
+
+    ]
+},
+{
+    name: "Meriadoc 'Merry' Brandybuck",
+    photo: "",
+    answers: [
+
+    ]
+},
+{
+    name: "Peregrin 'Pippin' Took",
+    photo: "",
+    answers: [
+
+    ]
+},
+{
+    
+}
+
+]
