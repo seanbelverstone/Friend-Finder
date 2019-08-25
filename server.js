0b99b197ce36f569119155e508a472aa5b407c04
Sean Belverstone <seanbelverstone@gmail.com>
2019-08-24 23:22:54 -0500

    Managed to get rid of one error by changing the htmlRoutes file, using use instead of get. Made some other changes but still doesnt work

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 var express = require("express");
+var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'friend-finder')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./app/routing/htmlRoutes.js")(app);
 require("./app/routing/apiRoutes")(app);
-
+require("./app/data/friends.js");
 
 app.listen(PORT, function() {
 
     console.log("App listening on PORT " + PORT);
 
 });
+
+
   
