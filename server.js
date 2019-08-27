440ea6c856b2e1c528d805ce8372717878f5e7a2
Sean Belverstone <seanbelverstone@gmail.com>
2019-08-27 16:28:42 -0500

    Noticed in my server.js file, id written the app.use section incorrectly. I needed to use the app folder in order for my css to function correctly. This is working now so im going to add some styling

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'friend-finder')));
+app.use(express.static(path.join(__dirname, 'app')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
