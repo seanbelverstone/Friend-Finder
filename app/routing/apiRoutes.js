b6f0b611bb6eec037294ff109240e4d2f98931a5
Sean Belverstone <seanbelverstone@gmail.com>
2019-09-01 11:43:11 -0500

    Added some more tests to try and get the popover to come up on the first click. Not working. WIll revisit later

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,47 +2,7 @@ var friends = require("../data/friends.js");
 
 module.exports = function(app) {
   // Return all friends found in friends.js as JSON
-  app.get("/api/friends", function(req, res) {
-    res.json(friends);
-  });
+  app.get("/api/friends", (request, response) => response.json(friends));
 
-  app.post("/api/friends", function(req, res) {
-
-    // Stores the user's details (name, photo and scores) into the variable 'user'
-    var user = req.body;
-
-    // ParseInt the scores
-    for(var i = 0; i < user.answers.length; i++) {
-      user.answers[i] = parseInt(user.scores[i]);
-    }
-
-    var bestFriendIndex = 0;
-    var minimumDifference = 40;
-
-
-    //Doing the ol' double for-loop. First we loop through the friends array. Then, we loop through my scores
-    //and take my scores away from the friends scores and saves it in totalDifference. I'm using Math.abs so
-    //I never get a negative number
-    for(var i = 0; i < friends.length; i++) {
-
-      var totalDifference = 0;
-
-      for(var j = 0; j < friends[i].answers.length; j++) {
-        var difference = Math.abs(user.answers[j] - friends[i].answers[j]);
-        totalDifference += difference;
-      }
-
-      //If the total difference is less than minimum, it sets the index to i so we can loop through again.
-      if(totalDifference < minimumDifference) {
-        bestFriendIndex = i;
-        minimumDifference = totalDifference;
-      }
-    }
-
-    //After a match has been found, push the user to the friends object.
-    friends.push(user);
-
-    //Sends the data back to the browser so we can do it again
-    res.json(friends[bestFriendIndex]);
-  });
+  app.post("/api/friends", (request, response) => console.log(request.body.answers));
 };
